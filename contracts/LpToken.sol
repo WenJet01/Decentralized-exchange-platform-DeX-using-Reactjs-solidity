9dfbe0cfa942e1efbfedc2d08e1aa9f562967ee9
WenJet01 <cheewj-wm19@student.tarc.edu.my>
2022-04-11 02:34:49 +0800

    Update LpToken.sol

--- contracts/LpToken.sol
+++ contracts/LpToken.sol
@@ -4,7 +4,11 @@ pragma solidity >=0.5.0 <0.9.0;
 contract LpToken {
     uint public totalSupply;
 
-    mapping(address => uint) public data;
+    struct providers{
+        uint token;
+        uint reward;
+    }
+    mapping(address => providers) public data;
     
 
 // babylonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
@@ -29,51 +33,20 @@ contract LpToken {
         totalSupply = sqrt(supply);
     }
 
-    function create(address _add, uint sbtProvided, uint totalSbt) public{
-        uint lp = calcLp(sbtProvided, totalSbt);
-        data[_add] = lp;
-    }
-
     function get(address _add)public view returns (uint){
-        return data[_add];
+        return data[_add].token;
     }
 
     function update(address _add, uint sbtProvided, uint totalSbt) public {
         uint lp = calcLp( sbtProvided, totalSbt);
-        data[_add] += lp;
+        data[_add].token += lp;
 
     }
 
     function minus(address _add, uint lpToken) public {
-        data[_add] -= lpToken;
+        data[_add].token -= lpToken;
 
     }
-
-    // function del(address _add)public {
-    //     bool found = false;
-    //     delete data[_add];
-
-    //     for(uint i=0 ;i < liqProviders.length; i++){
-    //         if(liqProviders[i] == _add){
-    //             for(uint j=i ;j < liqProviders.length; j++){
-                    
-    //                 if(j+1 == liqProviders.length){
-    //                     liqProviders.pop();
-    //                 }else{
-    //                     liqProviders[j] = liqProviders[j+1];
-    //                 }
-
-    //             }
-    //             found = true;
-    //         }
-
-    //         if(found){
-    //             break;
-    //         }
-            
-    //     }
-        
-    // }
 
     
 }
